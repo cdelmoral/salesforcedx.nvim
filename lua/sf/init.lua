65ce9234839590f7ac73e9f12208e9345a1c87b2
Carlos del Moral <carderomad@gmail.com>
2024-02-15 22:34:10 -0600

    feat: Add apex test class command (#2)
    Add function to run currently selected apex class tests.
    
    Also add vim command :TestClass to invoke it.

--- lua/sf/init.lua
+++ lua/sf/init.lua
@@ -5,10 +5,31 @@ local salesforce = require("sf.salesforce")
 
 M = {}
 
+M.execute_test_class = function()
+	local current_node = ts_utils.get_node_at_cursor()
+
+	if not current_node then
+		vim.api.nvim_err_writeln("Not an apex test class")
+		return
+	end
+
+	local class_node = apex.get_class_declaration_node(current_node)
+	if not apex.is_test(class_node) then
+		vim.api.nvim_err_writeln("Not an apex test class")
+		return
+	end
+	local class_name = apex.get_identifier_name(class_node)
+
+	local test_command = salesforce.get_apex_run_test(class_name)
+	local bufnr = utils.create_split()
+	utils.execute_command(test_command, bufnr)
+end
+
 M.execute_test_method = function()
 	local current_node = ts_utils.get_node_at_cursor()
 
 	if not current_node then
+		vim.api.nvim_err_writeln("Not an apex test class")
 		return
 	end
 
@@ -33,6 +54,7 @@ end
 
 M.setup = function()
 	vim.api.nvim_command("command! TestMethod lua require'sf'.execute_test_method()<CR>")
+	vim.api.nvim_command("command! TestClass lua require'sf'.execute_test_class()<CR>")
 end
 
 return M
